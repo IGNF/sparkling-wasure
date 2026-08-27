1caa863ebfda487f6546807f900a870208b88f6b
LBilheran <loisbilheran@outlook.fr>
2026-08-27 11:10:57 +0200

    remove: do_color part and do_lods part

--- run_workflow.sh
+++ run_workflow.sh
@@ -3,8 +3,6 @@
 
 export DDT_MAIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")/" && pwd )"
 BUILDS_DIR="${DDT_MAIN_DIR}/build/"
-DO_COLORIZE="FALSE"
-DO_LOD="TRUE"
 mkdir -p  /tmp/spark-events
 
 # Function to display usage
@@ -34,7 +32,6 @@ function run_algo_docker
 while [[ "$#" -gt 0 ]]; do
   case $1 in
       --input_dir) INPUT_DIR="${2%/}/"; shift ;;
-      --colorize) DO_COLORIZE="TRUE" ;;
       --debug) DEBUG_FLAG="-d" ;;
       --params) PARAMS="${2%/}/"; shift ;;
       --output_dir) OUTPUT_DIR="${2%/}/"; shift ;;
@@ -57,20 +54,5 @@ INPUT_DIR=${OUTPUT_DIR}
 PARAMS="${OUTPUT_DIR}/wasure_metadata_3d_gen.xml"
 FILE_SCRIPT="${DDT_MAIN_DIR}/services/wasure/workflow/workflow_wasure.scala"
 run_algo_docker
-
-CURRENT_CONDA_ENV=$(conda info --envs | grep '*' | awk '{print $1}') 
-
-
-TILE_DIR=${OUTPUT_DIR}/outputs/tiles/
-if [[ ${DO_COLORIZE} == "TRUE" ]]; then
-    TILE_DIR=${OUTPUT_DIR}/colorized_tiles
-    ${DDT_MAIN_DIR}/services/colorize/colorize.sh --input_dir ${LAZ_INPUT_DIR} --output_dir ${OUTPUT_DIR}
-fi
-
-if [[ ${DO_LOD} == "TRUE" ]]; then
-    echo -e "\n -[Create LODs from tiled mesh]-"
-    mkdir -p ${OUTPUT_DIR}/LODs
-    ${DDT_MAIN_DIR}/services/mesh23dtile/run.sh --input_dir ${TILE_DIR} --xml_file ${PARAMS} --output_dir ${OUTPUT_DIR}/LODs
-fi
 
 exit 0
